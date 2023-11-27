d37524d5d743ec2ea010b80d521b25d337cd6044
bianhaowen <bhaowen@qq.com>
2023-11-27 20:45:51 +0800

    bugfix
    Fixed the issue of duplicate vectors in layers other than the bottom layer.
    Fixed the issue of premature termination during the search for nearest neighbors.
    Fixed an issue in the distance calculation function where temporary variables were not aligned in memory.
    More detailed output.
    More TODO lists.

--- example/example.cpp
+++ example/example.cpp
@@ -35,7 +35,6 @@ std::vector<std::vector<float>> load_vector(const char *file_path)
     uint64_t dimension = 0;
     vectors_file.read((char *)&count, sizeof(uint64_t));
     vectors_file.read((char *)&dimension, sizeof(uint64_t));
-    //    std::cout << count << "  " << dimension << std::endl;
     auto vectors = std::vector<std::vector<float>>(count, std::vector<float>(dimension, 0));
     for (auto i = 0; i < count; ++i)
     {
@@ -61,7 +60,6 @@ std::vector<std::vector<uint64_t>> load_neighbors(const char *file_path)
     uint64_t neighbor_count = 0;
     neighbors_file.read((char *)&count, sizeof(uint64_t));
     neighbors_file.read((char *)&neighbor_count, sizeof(uint64_t));
-    //    std::cout << count << "  " << neighbor_count << std::endl;
     auto neighbors = std::vector<std::vector<uint64_t>>(count, std::vector<uint64_t>(neighbor_count, 0));
     for (auto i = 0; i < count; ++i)
     {
@@ -86,89 +84,45 @@ int main(int argc, char **argv)
     std::cout << "no SIMD supported. " << std::endl;
 #endif
     auto train = load_vector(argv[1]);
-    //    for (auto &i : vectors)
-    //    {
-    //        for (auto &j : i)
-    //        {
-    //            std::cout << j << "  ";
-    //        }
-    //        std::cout << std::endl;
-    //    }
-    //    std::cout << vectors.size() << "  " << vectors.begin()->size() << std::endl;
     auto test = load_vector(argv[2]);
-    //    for (auto &i : query)
-    //    {
-    //        for (auto &j : i)
-    //        {
-    //            std::cout << j << "  ";
-    //        }
-    //        std::cout << std::endl;
-    //    }
-    //    std::cout << query.size() << "  " << query.begin()->size() << std::endl;
     auto neighbors = load_neighbors(argv[3]);
-    //    for (auto &i : neighbors)
-    //    {
-    //        for (auto &j : i)
-    //        {
-    //            std::cout << j << "  ";
-    //        }
-    //        std::cout << std::endl;
-    //    }
-    //    std::cout << neighbors.size() << "  " << neighbors.begin()->size() << std::endl;
-    //    for (int i = 0; i < query.size(); ++i)
-    //    {
-    //        auto begin = std::chrono::high_resolution_clock::now();
-    //        auto query_result = bruteforce::search<float>(vectors, query[i], neighbors[i].size());
-    //        auto end = std::chrono::high_resolution_clock::now();
-    //        std::cout << "one query(ms): " << std::chrono::duration_cast<std::chrono::milliseconds>(end -
-    //        begin).count()
-    //                  << std::endl;
-    //        auto hit = verify(neighbors[i], query_result);
-    //        std::cout << "recall: " << hit << std::endl;
-    //    }
     auto begin = std::chrono::high_resolution_clock::now();
-    nnhnsw::Index<float> index(train, Distance_Type::Euclidean2, 20, 1);
+    nnhnsw::Index<float> index(train, Distance_Type::Euclidean2, 10, 1);
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << "building index costs(us): "
               << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
-    auto hit_results = std::vector<uint64_t>();
+    uint64_t total_hit = 0;
     for (auto i = 0; i < test.size(); ++i)
     {
-        auto begin = std::chrono::high_resolution_clock::now();
-        auto query_result = nnhnsw::query<float>(index, test[i], 10);
-        auto end = std::chrono::high_resolution_clock::now();
+        begin = std::chrono::high_resolution_clock::now();
+        auto query_result = nnhnsw::query<float>(index, test[i], neighbors[i].size());
+        end = std::chrono::high_resolution_clock::now();
         std::cout << "one query costs(us): "
                   << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
         auto hit = verify(neighbors[i], query_result);
-        hit_results.push_back(hit);
+        total_hit += hit;
         std::cout << "hit: " << hit << std::endl;
     }
-    {
-        uint64_t total_hit = 0;
-        for (auto &hit : hit_results)
-        {
-            total_hit += hit;
-        }
-        std::cout << "total hit: " << total_hit << std::endl;
-    }
-    //    for (auto i = 0; i < 10; ++i)
+    std::cout << "total hit: " << total_hit << std::endl;
+
+    // debug
     //    {
-    //        auto begin = std::chrono::high_resolution_clock::now();
-    //        auto query_result = nnhnsw::query<float>(index, train[i], 10);
-    //        auto end = std::chrono::high_resolution_clock::now();
-    //        std::cout << "one query costs(us): "
-    //                  << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
-    //        auto hit = verify(std::vector<uint64_t>(1, i), query_result);
-    //        hit_results.push_back(hit);
-    //        std::cout << "hit: " << hit << std::endl;
-    //    }
-    //    {
-    //        uint64_t total_hit = 0;
-    //        for (auto &hit : hit_results)
+    //        for (auto i = 0; i < 10; ++i)
     //        {
+    //            auto begin = std::chrono::high_resolution_clock::now();
+    //            auto query_result = nnhnsw::query<float>(index, train[i], 100);
+    //            auto end = std::chrono::high_resolution_clock::now();
+    //            std::cout << "one query costs(us): "
+    //                      << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+    //            auto hit = verify(std::vector<uint64_t>(1, i), query_result);
     //            total_hit += hit;
+    //            std::cout << "hit: " << hit << std::endl;
     //        }
-    //        std::cout << "total hit: " << total_hit << std::endl;
+    //        {
+    //            std::cout << "total hit: " << total_hit << std::endl;
+    //        }
     //    }
+    // debug
+
     return 0;
 }
