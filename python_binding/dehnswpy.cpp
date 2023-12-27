6d9197f7475b89b297cc08234f121971fed4f0c4
bianhaowen <bhaowen@qq.com>
2023-12-27 20:05:27 +0800

    bugfix

--- python_binding/dehnswpy.cpp
+++ python_binding/dehnswpy.cpp
@@ -9,13 +9,19 @@
 namespace py = pybind11;
 using namespace pybind11::literals; // needed to bring in _a literal
 
+void wrapped_insert(dehnsw::Index &index, const py::object &vector)
+{
+    py::array_t<float, py::array::c_style | py::array::forcecast> items(vector);
+    dehnsw::insert(index, items.data(0));
+}
+
 py::array_t<uint64_t> wrapped_query(const dehnsw::Index &index, const py::object &query_vector, uint64_t top_k,
                                     uint64_t relaxed_monotonicity = 0)
 {
     py::array_t<float, py::array::c_style | py::array::forcecast> items(query_vector);
+    auto result = dehnsw::query(index, items.data(0), top_k, relaxed_monotonicity);
     auto return_result = py::array_t<uint64_t>(top_k);
     auto return_buffer = (float *)return_result.request().ptr;
-    auto result = dehnsw::query(index, items.data(0), top_k, relaxed_monotonicity);
     for (int i = top_k - 1; i >= 0; i--)
     {
         return_buffer[i] = result.top().second;
@@ -33,6 +39,6 @@ PYBIND11_MODULE(dehnswpy, index)
         .value("Euclidean2", Distance_Type::Euclidean2)
         .value("Inner_Product", Distance_Type::Inner_Product)
         .value("Cosine_Similarity", Distance_Type::Cosine_Similarity);
-    index.def("insert", &dehnsw::insert, "Insert a vector into index.");
+    index.def("insert", &wrapped_insert, "Insert a vector into index.");
     index.def("query", &wrapped_query, "query the most k-th nearest neighbors of a vector.");
 }
