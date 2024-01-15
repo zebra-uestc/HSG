c893796691edc17db700e4a79ed7ea8a27ea2ae1
bianhaowen <bhaowen@qq.com>
2024-01-15 13:44:51 +0800

    feature
    Fix the issue in the 'Python' use case.

--- example/example.py
+++ example/example.py
@@ -18,19 +18,49 @@ def calculate_distance(vector1: numpy.ndarray, vector2: numpy.ndarray) -> float:
 
 
 def get_refernece_answer(train: numpy.ndarray, test: numpy.ndarray, neighbors: numpy.ndarray) -> numpy.ndarray:
-    reference: numpy.empty((len(test), len(neighbors)), dtype='float32')
+    answer = numpy.empty(shape=(len(test), len(neighbors[0])), dtype=float)
     for i in range(len(test)):
-        for j in range(len(neighbors)):
-            reference[i][j] = calculate_distance(test[i], train[neighbors[i][j]])
-    return reference
+        for j in range(len(neighbors[i])):
+            answer[i][j] = calculate_distance(test[i], train[neighbors[i][j]])
+    return answer
 
 
-def verify(reference_answer: numpy.ndarray, test: int, results: numpy.ndarray) -> int:
+def verify(reference_answer: numpy.ndarray, number: int, results: numpy.ndarray, train: numpy.ndarray,
+           test: numpy.ndarray) -> int:
     hit = 0
-    for distance in reference_answer[test]:
-        if (results[hit] <= distance):
+    for distance in reference_answer[number]:
+        if (calculate_distance(train[results[hit]], test[number]) <= distance):
             hit += 1
     return hit
+
+
+def performence_test(train: numpy.ndarray, test: numpy.ndarray, neighbors: numpy.ndarray,
+                     reference_answer: numpy.ndarray):
+    rf = open('dehnswpy.txt', 'w', encoding='utf-8')
+    connects = [4, 5, 6, 7, 8]
+    steps = [2, 3, 4]
+    querys = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+    for connect in connects:
+        for step in steps:
+            index = dehnswpy.Index(dehnswpy.Distance_Type.Euclidean2, int(len(train[0])), int(connect),
+                                   int(128),
+                                   int(step), 1000000000)
+            for i in range(len(train)):
+                dehnswpy.insert(index, train[i])
+            index = dehnswpy.optimize(index)
+            for query in querys:
+                total_time = 0
+                total_hit = 0
+                for i in range(len(test)):
+                    start = time.time()
+                    results = dehnswpy.query(index, test[i], len(neighbors[i]), query)
+                    end = time.time()
+                    total_time += end - start
+                    total_hit += verify(reference_answer, i, results)
+                rf.write("{:7d}".format(total_hit))
+                rf.write("    ")
+                rf.write("{}".format(total_time * 1000000 / len(test)))
+                rf.write("\n")
 
 
 def dataset_transform(dataset: h5py.Dataset) -> Tuple[
@@ -66,34 +96,34 @@ def dataset_transform(dataset: h5py.Dataset) -> Tuple[
     return numpy.array(dataset["train"]), numpy.array(dataset["test"]), numpy.array(dataset["neighbors"])
 
 
-rf = open('faiss_hnsw.txt', 'w', encoding='utf-8')
 # 数据集路径
 # 使用时需要修改
 f = h5py.File('/root/host/data/fashion-mnist/fashion-mnist-784-euclidean.hdf5', 'r')
 train, test, neighbors = dataset_transform(f)
 reference_answer = get_refernece_answer(train, test, neighbors)
-n, d = len(train), len(train[0])
-connects = [4, 5, 6, 7, 8]
-steps = [2, 3, 4]
-querys = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
-for connect in connects:
-    for step in steps:
-        index = dehnswpy.Index(dehnswpy.Distance_Type.Euclidean2, int(len(train[0])), int(connect),
-                               int(128),
-                               int(step), 1000000000)
-        for i in range(len(train)):
-            dehnswpy.insert(index, train[i])
-        index = dehnswpy.optimize(index)
-        for query in querys:
-            total_time = 0
-            total_hit = 0
-            for i in range(len(test)):
-                start = time.time()
-                results = dehnswpy.query(index, test[i], len(neighbors[i]), query)
-                end = time.time()
-                total_time += end - start
-                total_hit += verify(reference_answer, i, results)
-            rf.write("{:7d}".format(total_hit))
-            rf.write("    ")
-            rf.write("{}".format(total_time * 1000000 / len(test)))
-            rf.write("\n")
+index = dehnswpy.Index(dehnswpy.Distance_Type.Euclidean2, int(len(train[0])), int(4),
+                       int(128),
+                       int(4), 1000000000)
+total_time = 0
+for i in range(len(train)):
+    start = time.time()
+    dehnswpy.insert(index, train[i])
+    end = time.time()
+    total_time += end - start
+    print("inserting one vector consts: ")
+    print((end - start) * 1000000)
+print("building index consts: ")
+print(total_time * 1000000)
+index = dehnswpy.optimize(index)
+total_time = 0
+total_hit = 0
+for i in range(len(test)):
+    start = time.time()
+    results = dehnswpy.query(index, test[i], len(neighbors[i]), 1)
+    end = time.time()
+    total_time += end - start
+    total_hit += verify(reference_answer, i, results, train, test)
+print("average time: ")
+print(total_time * 1000000 / len(test))
+print("total hit: ")
+print(total_hit)
