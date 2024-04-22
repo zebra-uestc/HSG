944341186713fce3ef0c0c0a18d038e3ac3cb92d
bianhaowen <bhaowen@qq.com>
2024-04-22 10:58:27 +0000

    refactor
    use 'vector' instead of 'hash'

--- r.sh
+++ r.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 data=fashion-mnist
-numactl --physcpubind=0-11 --localalloc ./binary/release/test ./data/${data}/train ./data/${data}/test ./data/${data}/neighbors
+numactl --cpunodebind==1 --localalloc ./binary/release/test ./data/${data}/train ./data/${data}/test ./data/${data}/neighbors
 # data=sift
-# numactl --physcpubind=0-11 --localalloc ./binary/release/test ./data/${data}/train ./data/${data}/test ./data/${data}/neighbors
+# numactl --cpunodebind==1 --localalloc ./binary/release/test ./data/${data}/train ./data/${data}/test ./data/${data}/neighbors
 # data=gist
-# numactl --physcpubind=0-11 --localalloc ./binary/release/test ./data/${data}/train ./data/${data}/test ./data/${data}/neighbors
+# numactl --cpunodebind==1 --localalloc ./binary/release/test ./data/${data}/train ./data/${data}/test ./data/${data}/neighbors
