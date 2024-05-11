364fc5d408c64f7d0dfdd3a4025c677b97c0193a
TimLee-sNickName <lzswjtu@outlook.com>
2024-05-11 19:32:40 +0800

    将id也保存成文件的形式

--- data/sift/generated_data.py
+++ data/sift/generated_data.py
@@ -9,19 +9,20 @@ def dataset_transform(dataset: h5py.Dataset) -> Tuple[
     return np.array(dataset["train"]), np.array(dataset["test"]), np.array(dataset["neighbors"])
 
 
-def save_diff_vectors(dataset_file, output_file, k):
+def save_diff_vectors(dataset_file, output_file, id_output_file, k):
     # 打开HDF5文件
     f = h5py.File(dataset_file, 'r')
-    # 调用dataset_transform函数，将HDF5文件的内容转换为训练集、测试集和邻居集
+    # 调用dataset_transform函数，将HDF5文件的内容转换为train集、test集和neighbors集
     train, test, neighbors = dataset_transform(f)
     print('数据集分类完成...\n')
-    # 将训练集和邻居集转换为集合，以便进行快速查找
-    train_set = set(tuple(i) for i in train)
+    # 将训练集和邻居集转换为字典，以便进行快速查找，键是元组形式的数据，值是数据的ID
+    train_dict = {tuple(i): idx for idx, i in enumerate(train)}
     neighbors_set = set(tuple(i) for i in neighbors)
     # 找出在训练集中但不在邻居集中的向量
-    diff_set = train_set - neighbors_set
+    diff_dict = {i: train_dict[i] for i in train_dict.keys() - neighbors_set}
     # 将结果转换回numpy数组
-    diff_array = np.array([list(i) for i in diff_set])
+    diff_array = np.array([list(i) for i in diff_dict.keys()])
+    diff_ids = list(diff_dict.values())
     print('开始提取...\n')
     # 将结果保存为二进制文件
     with open(output_file, 'wb') as file:
@@ -36,5 +37,8 @@ def save_diff_vectors(dataset_file, output_file, k):
             count += 1
             print('生成',count,'条数据...\n')
             print('第',count,'条数据为：',i,'\n')
+    with open(id_output_file, 'wb') as file:
+        for id in diff_ids[:k]:
+            file.write(struct.pack("Q", id))
 # 使用函数
-save_diff_vectors('sift-128-euclidean.hdf5', 'diff', 100)
+save_diff_vectors('sift-128-euclidean.hdf5', 'diff', 'diff_ids', 100)
