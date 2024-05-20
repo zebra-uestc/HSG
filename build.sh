b137c8d6082adc64f874391b789d2080ef64dbd3
bianhaowen <bhaowen@qq.com>
2024-05-20 08:22:10 +0000

    clean
    Delete unnecessary files.
    Delete outdated content from readme.
    Modify the script.

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo ">>>>>>>>>>>>>>>>>>>>"
-echo "building \"release\""
-echo
-cmake -S . -B build -G"Ninja Multi-Config" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Release
-echo
-echo "build 'release' done"
-echo ">>>>>>>>>>>>>>>>>>>>"
+cmake -S . -B build -G"Ninja Multi-Config" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
 
-# echo "building \"debug\""
-# cmake -S . -B debug -G"Ninja Multi-Config" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_BUILD_TYPE=Debug
-# echo "build 'debug' done"
+# cmake -S . -B debug -G"Ninja Multi-Config" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc
