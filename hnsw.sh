2ff15c9dd8b66bb59a39dd72c560a6e41bb525b3
BHAOWEN <68286560+BHAOWEN@users.noreply.github.com>
2024-05-20 15:26:05 +0800

    Update hnsw.sh

--- hnsw.sh
+++ hnsw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-data=fashion-mnist
+# data=fashion-mnist
 numactl --physcpubind=12-23 --localalloc ./binary/release/hnsw ./data/${data}/train ./data/${data}/test ./data/${data}/neighbors
 # data=sift
 # ./binary/release/hnsw ./data/${data}/train ./data/${data}/test ./data/${data}/neighbors
