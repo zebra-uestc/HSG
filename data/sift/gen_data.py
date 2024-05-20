9633ae120d9fb0755d6e90061dede4ef4860f021
TimLee-sNickName <lzswjtu@outlook.com>
2024-05-20 15:40:14 +0800

    实现了三个方法

--- data/sift/gen_data.py
+++ data/sift/gen_data.py
@@ -50,7 +50,7 @@ def get_mixed_indices(neighbors, train_size, k):
     sum = 0
     # 记录每行剩余的元素个数
     array = np.full(neighbors.shape[0],neighbors.shape[1])
-    # 开始抽取元素
+    # 开始抽取元素，直至抽取总数达到k/2
     while(sum < k/2 and np.all(array >= 10)):
         willApend = True
         # 从所有元素中随机抽取一个
@@ -91,9 +91,9 @@ def get_mixed_indices(neighbors, train_size, k):
     return mixed_indices
 
 
-def half_of_topk_indices(neighbors, train_size,k):
+def get_topk_indices(neighbors,k):
     # 创建一个空数组用于存储邻居索引
-    half_of_topk_indices = np.empty((0))
+    _topk_indices = np.empty((0))
     # 存储所有被抽到的元素
     element_list = []
     # 已经抽取的元素总个数
@@ -101,20 +101,27 @@ def half_of_topk_indices(neighbors, train_size,k):
     # 记录每行剩余的元素个数
     array = np.full(neighbors.shape[0],neighbors.shape[1])
     # 开始抽取元素
-
+    for i in range(k):
+        top_k_elements = neighbors[:, i:i+1]
+        for j in top_k_elements.flatten():
+            # j没有被抽到过，则开始判断是否会导致某行少于10
+            if j not in element_list:
+                # 在neighbors集中，找到该被抽取到的元素所在行
+                rows, _ = np.where(neighbors == j)
+                for row in rows:
+                    if (array[row] - 1 < 10):
+                        print("该数据集最多只能选取top",k-1,"个索引...\n")
+                        return
+        # 如果所有j都不会导致某行少于10，则将此次选择的结果加入列表
+        element_list.extend(top_k_elements.tolist())
+        print("已选择",i+1,"列...\n")
     # 将列表转换为np数组
-    neighbors_sample = np.array(element_list)
+    topk_indices = np.array(list(set(element_list)))
     # 保存为二进制文件
-    with open('mixed_indices.bin', 'wb') as f:
-        f.write(mixed_indices.tobytes())
+    with open('topk_indices.bin', 'wb') as f:
+        f.write(topk_indices.tobytes())
     print("二进制文件保存成功...\n")
-    '''
-    加载二进制文件，恢复为原来的数组
-    with open('mixed_indices.bin', 'rb') as f:
-        loaded_indices = np.frombuffer(f.read(), dtype=non_neighbors_indices.dtype)
-    print("加载完毕")
-    '''
-    return half_of_topk_indices
+    return topk_indices
 
 k = 100
 # # 调用dataset_transform函数，将HDF5文件的内容转换为训练集、测试集和邻居集
@@ -124,3 +131,5 @@ train, test, neighbors = dataset_transform(f)
 non_neighbors_indices = get_non_neighbors_indices(neighbors, len(train), k)
 # k/2个非邻居索引，k/2个邻居索引
 mixed_indices = get_mixed_indices(neighbors, len(train), k)
+# 每行前k个
+topk_indices = get_topk_indices(neighbors,50)
