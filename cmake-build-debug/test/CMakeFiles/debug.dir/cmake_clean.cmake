1d2f15ece1dc7eafade842d29c7ac53a5bf09dcc
TimLee-sNickName <lzswjtu@outlook.com>
2024-05-22 11:17:18 +0800

    未跟踪文件

---
+++ cmake-build-debug/test/CMakeFiles/debug.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../binary/debug/debug"
+  "../../binary/debug/debug.pdb"
+  "CMakeFiles/debug.dir/debug.cpp.o"
+  "CMakeFiles/debug.dir/debug.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/test/CMakeFiles/hnsw.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../binary/debug/hnsw"
+  "../../binary/debug/hnsw.pdb"
+  "CMakeFiles/hnsw.dir/hnsw.cpp.o"
+  "CMakeFiles/hnsw.dir/hnsw.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hnsw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
